0124db5789af357d2197fc6b358b11b2c3ea4c9d
Luan Nascimento <164895664+nascmentoluan@users.noreply.github.com>
2025-02-12 14:25:17 -0300

    correção: implementando função

--- desafio_em_python_01/contador_de_letras.py
+++ desafio_em_python_01/contador_de_letras.py
@@ -1,6 +1,7 @@
 
-print('Inicializando programa contador de letras. . .')
-
-nome_digitado = input('Por favor, insira o seu nome: ')
-
-print(f'O nome {nome_digitado} tem {len(nome_digitado)} letras')
+def conta_letras():
+    print('Inicializando programa contador de letras. . .')
+    nome_digitado = input('Por favor, insira o seu nome: ')
+    return print(f'O nome {nome_digitado} tem {len(nome_digitado)} letras')
+    
+conta_letras()
