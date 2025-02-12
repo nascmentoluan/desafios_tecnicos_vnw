818e35746ccc313967e999f5fb779c66f0260565
Luan Nascimento <164895664+nascmentoluan@users.noreply.github.com>
2025-02-12 14:26:01 -0300

    corrigindo implementação da função

--- desafio_em_python_01/duplicador_de_numeros.py
+++ desafio_em_python_01/duplicador_de_numeros.py
@@ -1,7 +1,7 @@
 
-print('Bem vindo(a) ao sistema duplicador de números!')
 
 def calcula_dobro_do_numero():
+  print('Bem vindo(a) ao sistema duplicador de números!')
   valor_inicial = int(input('Por favor, insira um número: '))
   valor_duplicado = valor_inicial * 2
   return print(f'O dobro de {valor_inicial} é {valor_duplicado}')
