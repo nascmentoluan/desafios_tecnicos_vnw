79f1072ce8b413d5248418a0b95557373722de6e
Luan Nascimento <164895664+nascmentoluan@users.noreply.github.com>
2025-02-12 14:20:43 -0300

    implementando função para exibir nomes

--- desafio_em_python_01/acesso_de_registro_dos_alunos.py
+++ desafio_em_python_01/acesso_de_registro_dos_alunos.py
@@ -1,8 +1,16 @@
 
 print('Acessando Registro de Alunos. . .')
 
-sistema_de_alunos = ['Ana', 'Bruno', 'Carla', 'Daniel', 'Eduardo']
 
-print(sistema_de_alunos[0])
+print('Acessando Registro de Alunos. . .')
 
-print(sistema_de_alunos[-1])
+def funcao_exibe_registros():
+
+    sistema_de_alunos = ('Ana', 'Bruno', 'Carla', 'Daniel', 'Eduardo')
+
+    print(sistema_de_alunos[0])
+
+    print(sistema_de_alunos[-1])
+    
+
+funcao_exibe_registros()
